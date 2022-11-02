2c26302bdbc668cd23f04745c49bab493080df45
Cyril-Dmrs <cyril.desmurs@outlook.com>
2022-11-02 15:49:31 +0100

    Ajout de la navbar

--- index.php
+++ index.php
@@ -6,10 +6,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Serial Screeners</title>
     <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/6d9b9bcbc0.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,400;0,600;1,400&display=swap" rel="stylesheet">
 </head>
 
 <body>
+<header>
+        <div id="accueil_titre_principal">
+            <div class="accueil_titre_1"><p>Serial Screeners</p></div>
+            </div>
+        </div>
 
+    <nav>
+        <ul>
+            <li><a href="">Accueil</a></li>
+            <li><a href="./series.html">Séries</a></li>
+            <li><a href="./films.html">Films</a></li>
+            <li><a href="./espace.html">Mon espace</a></li>
+        </ul>
+    </nav>
+    
+    <div class="btn-account">
+        <a href="./compte.html"><i class="fa-solid fa-user-large"></i></a>
+    </header>
+
+    <div class="accueil-scotch"><img src="./yellow scotch.jpg"></div>
 </body>
 
 </html>
