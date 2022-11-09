0de9d5ba8ada7e579da79cbe1add5694a17c844b
Margelle84 <bebelmon@hotmail.fr>
2022-11-09 16:14:06 +0100

    MAJ Ben Index + Style

--- index.php
+++ index.php
@@ -4,16 +4,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta description="Le site collaboratif qui répertorie les films et séries"
+    content="série, télévision, films, cinéma, divertissement, culture, comédie, drame, thriller, action, horreur, JustWatch, blockbuster, imaginaire, caméra">
     <title>Serial Screeners</title>
     <link rel="stylesheet" href="style.css">
     <script src="https://kit.fontawesome.com/6d9b9bcbc0.js" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,400;0,600;1,400&display=swap" rel="stylesheet">
+    <link rel="icon" href="./serial screeners/img/favicon-256x256.png">
 </head>
 
 <body>
-<header>
+    <header> <!-- Début du header, navbar et connexion inscription -->
         <div id="accueil_titre_principal">
             <div class="accueil_titre_1"><p>Serial Screeners</p></div>
             </div>
@@ -27,12 +30,44 @@
             <li><a href="./espace.html">Mon espace</a></li>
         </ul>
     </nav>
-    
+
     <div class="btn-account">
         <a href="./compte.html"><i class="fa-solid fa-user-large"></i></a>
-    </header>
+        
+    </header><!-- Fin du header, navbar et connexion inscription -->
 
-    <div class="accueil-scotch"><img src="./img/yellow scotch.jpg"></div>
+    <!-- Début bannière logo et slogan -->
+    <div id="accueil-banniere">
+    <div class="accueil-scotch-top"></div>
+
+    <div id="accueil-slogo">
+        <div class="accueil-logo"> <img src="./serial screeners/img/logo.jpg" alt=""></div>
+        <div class="accueil-slogan"> <img src="./serial screeners/img/slogan.jpg"></div>
+    </div>
+
+    <div class="accueil-scotch-bottom"></div>
+    </div>  <!-- Fin bannière logo et slogan -->
+
+    <!-- Début carrousel accueil -->
+    <div class="container">
+        <div class="carousel">
+          <div class="carousel__face"></div>
+          <div class="carousel__face"></div>
+          <div class="carousel__face"></div>
+          <div class="carousel__face"></div>
+          <div class="carousel__face"></div>
+          <div class="carousel__face"></div>
+          <div class="carousel__face"></div>
+          <div class="carousel__face"></div>
+          <div class="carousel__face"></div>
+        </div> 
+      </div> <!-- Fin carrousel accueil -->
+
+      <footer>      
+        <div class="mentleg">
+        <p><a href="./mentleg.html">Mentions légales</a> - Tous droits réservés © The DéléDevs</p>
+        </div>
+        </footer>
+        
 </body>
-
 </html>
