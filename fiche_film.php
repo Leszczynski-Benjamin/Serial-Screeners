ab485dc35985d67dbb19f30c07da8decf220baeb
Benjamin Leszczynski <leszczynskibenjamin@gmail.com>
2022-11-14 10:39:23 +0100

    feat:update

--- fiche_film.php
+++ fiche_film.php
@@ -24,7 +24,6 @@ while ($data = $req->fetch()) :
 
     </head>
 
-
     <body>
         <div class="container_fiche">
             <!--COLONNE DE GAUCHE, AFFICHE ET NOTE GENERALE DU FILM -->
@@ -33,10 +32,10 @@ while ($data = $req->fetch()) :
 
                 <h4 class="box">
                     <i class="fa-solid fa-star"></i>
-                    <div class="note"><?= $data->note ?></div>
+                    <div class="note">
+                        <?= $data->note ?>
+                    </div>
                 </h4>
-
-
             </div>
             <!-- COLONNE DE DROITE, TITRE ET INFOS DIVERSES -->
             <div class="right">
@@ -54,67 +53,78 @@ while ($data = $req->fetch()) :
                 <!-- SECTION LEFT BIS N'APPARAIT QUE SUR LA VERSION MOBILE, C'EST L'AFFICHE ET LA NOTE LORSQU'ELLES S'INTERCALENT ENTRE LE TITRE ET LES INFOS DIVERSES -->
                 <div class="left_bis">
                     <img class="affiche" src="./img/films/pulp_fiction.jpg" alt="affiche">
-
                     <h4 class="box">
                         <i class="fa-solid fa-star"></i>
-                        <div class="note"><?= $data->note ?></div>
+                        <div class="note">
+                            <?= $data->note ?>
+                        </div>
                     </h4>
-
-
                 </div>
-
-
+                <!-- CONTAINER1 CORRESPOND AUX INFOS REALISATEUR, GENRE ETC DIVISÉ EN DEUX COLONNES LEFT1 ET RIGHT1  -->
                 <div class="container1">
                     <div class="left1">
-                        <h5 class="cast">Réalisateur(s): <?= $data->realisateur ?></h5>
-                        <h5 class="cast">Acteurs principaux: <?= $data->acteurs_principaux ?></h5>
+                        <h5 class="cast">Réalisateur(s):
+                            <?= $data->realisateur ?>
+                        </h5>
+                        <h5 class="cast">Acteurs principaux:
+                            <?= $data->acteurs_principaux ?>
+                        </h5>
                     </div>
                     <div class="right1">
-                        <h5 class="infos">Genre: <?= $data->genre ?></h5>
-                        <h5 class="infos">Date de sortie: <?= $data->date_de_sortie_france ?>
-                            <h5 class="infos">Durée: <?= $data->duree_en_minutes ?> minutes
-                            </h5>
-                    </div>
-                    <p class="description"><?= $data->synopsis ?></p>
-
-                    <div class="bouton">
-                        <button><i class="fa-solid fa-circle-plus"></i> Ajouter à ma liste</button>
-                    </div>
-                    <div class="diffusion">
-
-                        <?php
-                        $cinema = $data->ou_voir_le_film;
-                        if ($cinema == 'En salles') {
-                            echo "<p class='lien'> En salles </p>";
-                        } else {
-                            echo "<a class='lien' href='$cinema'>Où visionner ce film?</a>";
-                        }
-                        ?>
+                        <h5 class="infos">Genre:
+                            <?= $data->genre ?>
+                        </h5>
+                        <h5 class="infos">Date de sortie:
+                            <?= $data->date_de_sortie_france ?>
+                        </h5>
+                        <h5 class="infos">Durée:
+                            <?= $data->duree_en_minutes ?> minutes
+                        </h5>
                     </div>
                 </div>
+                <!-- SYNOPSIS, BOUTON AJOUTER A LA LISTE ET LIEN DE DIFFUSION  -->
+                <p class="description">
+                    <?= $data->synopsis ?>
+                </p>
 
-                <div class="titre_avis">
-                    <h2>Avis</h2>
+                <div class="bouton">
+                    <button><i class="fa-solid fa-circle-plus"></i> Ajouter à ma liste</button>
                 </div>
+                <div class="diffusion">
 
-                <div class="avis">
-                    <div class="stars">
-                        <p class="noter">Donner une note</p>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-regular fa-star"></i>
-                        <i class="fa-regular fa-star"></i>
-                    </div>
-                    <form action="com_exe.php" method="post">
-                        <textarea class="commentaire" name="com" id="com" cols="70" rows="10" placeholder="Postez un avis ici..."></textarea>
-                        <button class="bouton" type="submit" class="poster">Soumettre</button>
-                    </form>
-                </div>
-                <div class="titre_avis">
-                    <h2>Avis</h2>
+                    <?php
+                    $cinema = $data->ou_voir_le_film;
+                    if ($cinema == 'En salles') {
+                        echo "<p class='lien'> En salles </p>";
+                    } else {
+                        echo "<a class='lien' href='$cinema'>Où visionner ce film?</a>";
+                    }
+                    ?>
                 </div>
             </div>
+
+            <!-- SECTION AVIS, SECONDE PARTIE DE LA PAGE, AVEC LES 5 ETOILES POUR NOTER ET UN FORMULAIRE POUR LAISSER UN COMMENTAIRE -->
+
+            <div class="titre_avis">
+                <h2>Avis</h2>
+            </div>
+
+            <div class="avis">
+                <div class="stars">
+                    <p class="noter">Donner une note</p>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                </div>
+                <form action="com_exe.php" method="post">
+                    <textarea class="commentaire" name="com" id="com" cols="70" rows="10" placeholder="Postez un avis ici..."></textarea>
+                    <button class="bouton" type="submit" class="poster">Soumettre</button>
+                </form>
+            </div>
+
+        </div>
 
 
     </body>
