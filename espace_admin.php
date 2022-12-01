d7a9430a10dddd098d84a923c1122d532f0c3c72
Benjamin Leszczynski <leszczynskibenjamin@gmail.com>
2022-12-01 10:24:11 +0100

    affichage pseudo utilisateur

--- espace_admin.php
+++ espace_admin.php
@@ -2,19 +2,18 @@
 
 include 'header.php';
 
-?>
 
-<h2 class="welcome">Bienvenue dans l'espace administrateur</h2>
+
+echo '<h2 class="welcome">Bienvenue dans l\'espace administrateur ' . $_SESSION["pseudo"] .  '</h2>
 
 <div class="links_box">
     <a class="link_admin" href="./create_film.php">Ajouter un nouveau film</a><br>
     <a class="link_admin" href="./create_serie.php">Ajouter une nouvelle série</a><br>
-    <a class="link_admin" href="./update.php">Modifier la fiche d'un film ou d'une série</a><br>
+    <a class="link_admin" href="./update.php">Modifier la fiche d\'un film ou d\'une série</a><br>
     <a class="link_admin" href="./submit_list.php">Consulter les suggestions</a><br>
     <a class="link_admin" href="./update_membre.php">Supprimer un membre</a><br>
-    <a class="link_admin" href="./index.php">Retour à l'accueil</a>
-</div>
+    <a class="link_admin" href="./index.php">Retour à l\'accueil</a>
+</div>';
 
-<?php
+
 include './footer.php';
-?>
