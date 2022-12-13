a0663e20db36c1ca4c23c68e1019497bd2583e02
Margelle84 <bebelmon@hotmail.fr>
2022-12-13 11:32:05 +0100

    delete com

---
+++ delete_com.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+
+$ID =$_GET['ID'];
+
+require "./database.php";
+
+$req = $pdo->prepare("DELETE FROM avis WHERE ID = ?");
+
+$req->execute(array($ID));
+
+exit(header('location: ./update.php'));
+
+?>
